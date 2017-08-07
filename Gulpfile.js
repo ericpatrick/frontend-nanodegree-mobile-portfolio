9c7c72152f878fb9436fe91e6f5819db3f11fced
Eric Patrick <ericpatrick15@gmail.com>
2017-08-06 22:33:57 -0300

    fix configurations

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,32 +3,9 @@ const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
 const imageResize = require('gulp-image-resize');
 const imagemin = require('gulp-imagemin');
-const del = require('del');
 const merge2 = require('merge2');
 const uglify = require('gulp-uglify');
-
-gulp.task("clean", function () {
-  return del([
-    "css/**/*.min*", 
-    "js/*.min*",
-    "img/*.otz*",
-    "views/css/**/*.min*",
-    "views/js/*.min*"
-  ]);
-});
-
-gulp.task("min-css", function () {
-  return merge2( 
-    gulp.src("css/**/*.css")
-      .pipe(cssmin())
-      .pipe(rename({suffix: ".min"}))
-      .pipe(gulp.dest('css')),
-    gulp.src("views/css/**/*.css")
-      .pipe(cssmin())
-      .pipe(rename({suffix: ".min"}))
-      .pipe(gulp.dest('views/css'))
-  );
-});
+const inline = require('gulp-inline');
 
 gulp.task("image-optimize", function () {
   return merge2(
@@ -41,7 +18,7 @@ gulp.task("image-optimize", function () {
       }))
       .pipe(imagemin())
       .pipe(rename({suffix: ".icon"}))
-      .pipe(gulp.dest("views/images")),
+      .pipe(gulp.dest("dist/views/images")),
     
     gulp.src("views/images/pizzeria.jpg")
       .pipe(imageResize({
@@ -52,27 +29,34 @@ gulp.task("image-optimize", function () {
       }))
       .pipe(imagemin())
       .pipe(rename({suffix: ".header"}))
-      .pipe(gulp.dest("views/images")),
+      .pipe(gulp.dest("dist/views/images")),
     gulp.src("img/*")
       .pipe(imagemin())
-      .pipe(rename({suffix: ".otz"}))
-      .pipe(gulp.dest("img"))
+      .pipe(gulp.dest("dist/img")),
+    gulp.src("views/images/pizza.png")
+      .pipe(imagemin())
+      .pipe(gulp.dest("dist/views/images"))
     );
 });
 
-gulp.task("js-uglify", function() {
-  return merge2(
-    gulp.src("js/*")
-      .pipe(uglify())
-      .pipe(rename({suffix: ".min"}))
-      .pipe(gulp.dest("js")),
-    gulp.src("views/js/*")
-      .pipe(uglify())
-      .pipe(rename({suffix: ".min"}))
-      .pipe(gulp.dest("views/js"))
-  );
+gulp.task("inline-resources", function() {
+  let folders = ["", "views/"];
+
+  let tasks = folders.map(function(folder) {
+    return gulp.src(folder + "*.html")
+      .pipe(inline({
+        css: function() {
+          return cssmin({keepSpecialComments : 0});
+        },
+        js: uglify,
+        disabledTypes: ['svg', 'img']
+      }))
+      .pipe(gulp.dest("dist/" + folder));
+  });
+  
+  return merge2(tasks);
 });
 
-gulp.task("default", ["clean"], function() {
-  gulp.run("image-optimize", "min-css", "js-uglify");
+gulp.task("default", function() {
+  gulp.run("image-optimize", "inline-resources");
 });
